8c663c5bc9ad69bc77bcb892af8953eaa321859a
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-09-23 00:58:27 +0200

    Cambio de color con dash

---
+++ Assets/Scripts/Player/PlayerColorController.cs
@@ -0,0 +1,52 @@
+﻿using Codetox.Variables;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerColorController : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+        public Variable<bool> canDash;
+        public Variable<bool> dashIsReady;
+
+        private void OnEnable()
+        {
+            canDash.OnValueChanged += CanDashOnValueChanged;
+            dashIsReady.OnValueChanged += DashIsReadyOnValueChanged;
+        }
+
+        private void OnDisable()
+        {
+            canDash.OnValueChanged -= CanDashOnValueChanged;
+            dashIsReady.OnValueChanged -= DashIsReadyOnValueChanged;
+        }
+
+        private void DashIsReadyOnValueChanged(bool condition)
+        {
+            if (condition && canDash.Value)
+            {
+                spriteRenderer.material.SetInt("_isColorChanged", 0);
+                spriteRenderer.material.SetInt("_NoDash", 0);
+            }
+            else
+            {
+                spriteRenderer.material.SetInt("_isColorChanged", 1);
+                spriteRenderer.material.SetInt("_NoDash", 1);
+            }
+        }
+
+        private void CanDashOnValueChanged(bool condition)
+        {
+            if (condition && dashIsReady.Value)
+            {
+                spriteRenderer.material.SetInt("_isColorChanged", 0);
+                spriteRenderer.material.SetInt("_NoDash", 0);
+            }
+            else
+            {
+                spriteRenderer.material.SetInt("_isColorChanged", 1);
+                spriteRenderer.material.SetInt("_NoDash", 1);
+            }
+        }
+    }
+}
