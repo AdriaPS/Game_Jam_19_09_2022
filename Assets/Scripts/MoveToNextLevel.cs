a58db6554f6e091f048c2df35c112cff60db0988
Shadowini <adriapujolsierra@gmail.com>
2022-09-25 17:50:46 +0200

    LevelSelection

---
+++ Assets/Scripts/MoveToNextLevel.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 7)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                SceneManager.LoadScene(nextSceneLoad);
+
+                if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+                }
+            }
+        }
+    }
+
+    public void GoToLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
