1668a4565024358f4fde3c8e30626ee5a5d02ef2
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-09-26 17:20:03 +0200

    Sonidos

---
+++ Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEvent : MonoBehaviour
+{
+    public UnityEvent onWalk;
+
+    public UnityEvent onJump;
+
+    public void OnWalk()
+    {
+        onWalk?.Invoke();
+    }
+
+    public void OnJump()
+    {
+        onJump?.Invoke();
+    }
+}
