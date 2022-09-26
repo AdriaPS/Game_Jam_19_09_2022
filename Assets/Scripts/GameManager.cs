c16175148a01dc85d7b86c02c123f644da822aca
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-09-26 16:26:05 +0200

    Menu de pausa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public UnityEvent onPause;
+    public UnityEvent onResume;
+
     public void RestartCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        onPause?.Invoke();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        onResume?.Invoke();
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
